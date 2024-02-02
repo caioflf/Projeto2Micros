425642c70e0952e48d9469857a75ee0c72d6023f
caioflf <102181753+caioflf@users.noreply.github.com>
2024-02-01 21:11:04 -0400

    Create atrasos.h

---
+++ atrasos.h
@@ -0,0 +1,37 @@
+/*=================================================================================================*/
+//	UNIVERSIDADE FEDERAL DO RIO GRANDE DO SUL
+//	ENG04475 - Microprocessadores I (2023/2)
+//	Projeto 2 - HOMICROS
+//
+// Alunos:
+// Bruno Gevehr Fernandes (00335299)
+// Caio Fernando Leite França (00330098)
+// Thiago Arndt Schimit (00333710)
+/*=================================================================================================*/
+
+#ifndef _ATRASOS_H_
+#define _ATRASOS_H_
+
+void atraso_40us();
+
+void atraso_1ms();
+
+void atraso_1ms64();
+
+void atraso_2ms();
+
+void atraso_15ms();
+
+void atraso_250ms();
+
+void atraso_500ms();
+
+void atraso_1s();
+
+void atraso_2s();
+
+void startContadorTempo();
+
+void stopContadorTempo();
+
+#endif
