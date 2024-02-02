adea74327d8a55b8ef1d0eaed5e2c4d0c80a6f94
caioflf <102181753+caioflf@users.noreply.github.com>
2024-02-01 21:18:28 -0400

    Create teclado.c

---
+++ teclado.c
@@ -0,0 +1,42 @@
+/*=================================================================================================*/
+//	UNIVERSIDADE FEDERAL DO RIO GRANDE DO SUL
+//	ENG04475 - Microprocessadores I (2023/2)
+//	Projeto 1 - UBERGS
+//
+// Alunos:
+// Bruno Gevehr Fernandes (00335299)
+// Caio Fernando Leite França (00330098)
+// Thiago Arndt Schimit (00333710)
+/*=================================================================================================*/
+
+#include "main.h"
+#include "teclado.h"
+
+
+unsigned char debounce(unsigned char num_bit){
+
+}
+
+// Funcao que verifica se é pressionada a tecla '*' por tempo suficiente
+unsigned char verificacao_tecla1(char tempo){ //tempo multiplicado por 2 em seg por conta de usar atraso de 0,5seg
+
+
+// Funcao que verifica se é pressionada a tecla '#' por tempo suficiente
+unsigned char verificacao_tecla2(char tempo){  // tempo em segundos (aproximado
+
+}
+
+//Funcao que verifia se é solicitado o logout, o desligue do sistema, ou a ligacao do sistema
+unsigned char verifica_login(){
+	
+}
+
+// Funcao qu escaneia as linhas 1, 2, 3 e 4
+unsigned char scan(unsigned char linha){
+
+}
+
+// Funcao para controlar o carro
+void movimento_manual (char letra){
+	
+}
